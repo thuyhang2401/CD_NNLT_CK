77557134e92a024fc9453870bcbb2c0297fb35e7
DungVT <143146290+PC-Evo@users.noreply.github.com>
2025-05-18 23:38:18 +0700

    [feature] front end created by Ms. Diep

---
+++ DataCrawlProject/app_main/front_end/js/script.js
@@ -0,0 +1,36 @@
+
+const productList = document.getElementById('product-list');
+productList.innerHTML = '<div class="loader"></div><p class="load">Loading...</p>'; // Hiển thị loading và dòng chữ
+fetch('http://127.0.0.1:9001/')
+    .then(response => response.json())
+    .then(data => {
+        productList.innerHTML = ''; // Xóa loading và dòng chữ
+        let productsHtml = '<div class="row">';
+
+        data.forEach((glasses, index) => {
+            if (index % 2 === 0 && index !== 0) {
+                productsHtml += '</div><div class="row">';
+            }
+            productsHtml += `
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <div class="card">
+                        <div class="card-img">
+                            <img src="${glasses.img_url}" alt="${glasses.glasses_name}">
+                        </div>
+                        <div class="card-title">
+                            <h3>Name: ${glasses.glasses_name}</h3>
+                        </div>
+                        <div class="card-brand">
+                            <p>Brand: <a href="${glasses.brand_url}" target="_blank">${glasses.brand_name}</a></p>
+                        </div>
+                        <div class="card-price">
+                            <p>Price: $${glasses.price}</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
+        productsHtml += '</div>';
+        productList.innerHTML = productsHtml;
+    })
+    .catch(error => console.log(error));
